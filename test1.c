6dd891616c1bb3b49a4907b167f84303ed6146ed
smj6052 <seominjung@gmail.com>
2021-02-02 20:27:06 +0900

    abc

--- test1.c
+++
@@ -1,9 +0,0 @@
-﻿1>------ 빌드 시작: 프로젝트: CuvicGame, 구성: Debug Win32 ------
-1>test.c
-1>C:\Users\서민정\source\repos\CuvicGame\SHISEN-SHO\test.c(40,17): warning C4101: 'fin' :참조되지 않은 지역 변수입니다.
-1>C:\Users\서민정\source\repos\CuvicGame\SHISEN-SHO\test.c(65,25): warning C4133: '함수': 'int [6]'과(와) 'char *' 사이의 형식이 호환되지 않습니다.
-1>C:\Users\서민정\source\repos\CuvicGame\SHISEN-SHO\test.c(76,18): warning C4244: '함수': 'time_t'에서 'unsigned int'(으)로 변환하면서 데이터가 손실될 수 있습니다.
-1>C:\Users\서민정\source\repos\CuvicGame\SHISEN-SHO\test.c(128,1): warning C4047: '초기화 중': 'int'의 간접 참조 수준이 'int *'과(와) 다릅니다.
-1>CuvicGame.vcxproj -> C:\Users\서민정\source\repos\CuvicGame\Debug\CuvicGame.exe
-1>"CuvicGame.vcxproj" 프로젝트를 빌드했습니다.
-========== 빌드: 성공 1, 실패 0, 최신 0, 생략 0 ==========
